5424de64d8db0ac9bfc3a4dad5019d5a0e902abe
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-04-12 17:12:48 +0700

    Initial notes-app-back-end starter

---
+++ notes-app-back-end/src/server.js
@@ -0,0 +1,21 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.36.19',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
