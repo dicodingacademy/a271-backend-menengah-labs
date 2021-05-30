6518f377a73b5df3df254ba4389eaec108e5e7fa
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-05-30 15:45:47 +0700

    change double quote to single quote

--- notes-app-back-end/src/api/notes/handler.js
+++ notes-app-back-end/src/api/notes/handler.js
@@ -1,4 +1,4 @@
-const ClientError = require("../../exceptions/ClientError");
+const ClientError = require('../../exceptions/ClientError');
 
 class NotesHandler {
   constructor(service, validator) {
