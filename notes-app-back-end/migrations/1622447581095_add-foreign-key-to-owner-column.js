00745c96bc6b0fe633fb2552b279b6db84d8bdef
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-05-31 15:15:27 +0700

    add foreign key to notes owner column

---
+++ notes-app-back-end/migrations/1622447581095_add-foreign-key-to-owner-column.js
@@ -0,0 +1,25 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  // membuat user baru.
+  pgm.sql("INSERT INTO users(id, username, password, fullname) VALUES ('old_notes', 'old_notes', 'old_notes', 'old notes')");
+
+  // mengubah nilai owner pada note yang owner-nya bernilai NULL
+  pgm.sql("UPDATE notes SET owner = 'old_notes' WHERE owner = NULL");
+
+  // memberikan constraint foreign key pada owner terhadap kolom id dari tabel users
+  pgm.addConstraint('notes', 'fk_notes.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  // menghapus constraint fk_notes.owner_users.id pada tabel notes
+  pgm.dropConstraint('notes', 'fk_notes.owner_users.id');
+
+  // mengubah nilai owner old_notes pada note menjadi NULL
+  pgm.sql("UPDATE notes SET owner = NULL WHERE owner = 'old_notes'");
+
+  // menghapus user baru.
+  pgm.sql("DELETE FROM users WHERE id = 'old_notes'");
+};
