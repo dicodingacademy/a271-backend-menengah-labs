a45acd58399de9ceb31da818c0b72d98ad4d96ce
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2021-06-04 14:22:37 +0700

    change production host to 0.0.0.0

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.36.19',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
