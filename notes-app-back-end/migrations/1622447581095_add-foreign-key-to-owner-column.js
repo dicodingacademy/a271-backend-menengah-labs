5eb33eee520904084a83c10e28afba4e17389be2
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2022-07-21 14:38:38 +0700

    Change NULL comparation to IS

--- notes-app-back-end/migrations/1622447581095_add-foreign-key-to-owner-column.js
+++ notes-app-back-end/migrations/1622447581095_add-foreign-key-to-owner-column.js
@@ -7,7 +7,7 @@ exports.up = (pgm) => {
   pgm.sql("INSERT INTO users(id, username, password, fullname) VALUES ('old_notes', 'old_notes', 'old_notes', 'old notes')");
 
   // mengubah nilai owner pada note yang owner-nya bernilai NULL
-  pgm.sql("UPDATE notes SET owner = 'old_notes' WHERE owner = NULL");
+  pgm.sql("UPDATE notes SET owner = 'old_notes' WHERE owner IS NULL");
 
   // memberikan constraint foreign key pada owner terhadap kolom id dari tabel users
   pgm.addConstraint('notes', 'fk_notes.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
