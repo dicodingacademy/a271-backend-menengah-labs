03a071cd29bdab44bcb3c0aeb1cd03e750f928eb
Aras <aras.opraza@gmail.com>
2023-11-02 23:52:07 +0700

    update dependencies version and change port to 3000

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
+    port: 3000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
