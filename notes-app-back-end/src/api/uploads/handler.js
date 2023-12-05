91d46ff509c487aad5d6d439aeeebc1daa3b536f
arasopraza <aras.opraza@gmail.com>
2023-12-05 09:59:00 +0700

    fix indent in handler

--- notes-app-back-end/src/api/uploads/handler.js
+++ notes-app-back-end/src/api/uploads/handler.js
@@ -10,16 +10,16 @@ class UploadsHandler {
     const { data } = request.payload;
     this._validator.validateImageHeaders(data.hapi.headers);
 
-      const fileLocation = await this._service.writeFile(data, data.hapi);
+    const fileLocation = await this._service.writeFile(data, data.hapi);
 
-      const response = h.response({
-        status: 'success',
-        data: {
-          fileLocation,
-        },
-      });
-      response.code(201);
-      return response;
+    const response = h.response({
+      status: 'success',
+      data: {
+        fileLocation,
+      },
+    });
+    response.code(201);
+    return response;
   }
 }
 
