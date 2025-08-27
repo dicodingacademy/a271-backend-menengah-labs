ef2f2920a1ca4e3f55a7c3aec30234bb199507ce
arasopraza <aras.opraza@gmail.com>
2025-08-27 10:32:27 +0700

    feat: add authentications

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -1,9 +1,13 @@
 import ClientError from './client-error.js';
 import InvariantError from './invariant-error.js';
 import NotFoundError from './not-found-error.js';
+import AuthorizationError from './authorization-error.js';
+import AuthenticationError from './authentication-error.js';
 
 export {
   ClientError,
   InvariantError,
   NotFoundError,
+  AuthorizationError,
+  AuthenticationError
 };
