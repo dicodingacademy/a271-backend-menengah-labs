27cff0ba4f7ca718be75909244dcfcb614389fee
arasopraza <aras.opraza@gmail.com>
2025-08-29 13:55:11 +0700

    fix: path export service

--- src/services/exports/controllers/export-controller.js
+++ src/services/exports/controllers/export-controller.js
@@ -1,5 +1,5 @@
 import response from '../../../utils/response.js';
-import ExportService from '../services/export-service.js';
+import ExportService from '../producers/export-service.js';
 
 export const exportNotes = async (req, res) => {
   const { targetEmail } = req.validated;
