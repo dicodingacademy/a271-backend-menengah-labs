0ab98b76ed768adfbe765d566c2a0806ec36f384
arasopraza <aras.opraza@gmail.com>
2025-09-18 21:15:39 +0700

    fix: error route users

--- src/services/users/routes/index.js
+++ src/services/users/routes/index.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { createUser, getUsers, getUserById, editUser, deleteUser } from '../controllers/user-controller.js';
 import validate from '../../../middlewares/validate.js';
-import authenticateToken from '../../../middlewares/auth.js';
 import { userPayloadSchema, userUpdatePayloadSchema } from '../../../services/users/validator/schema.js';
 
 const router = Router();
@@ -10,9 +9,9 @@ const router = Router();
 router.post('/', validate(userPayloadSchema), createUser);
 
 // Protected routes - Require authentication
-router.get('/', authenticateToken, getUsers);
-router.get('/:id', authenticateToken, getUserById);
-router.put('/:id', authenticateToken, validate(userUpdatePayloadSchema), editUser);
-router.delete('/:id', authenticateToken, deleteUser);
+router.get('/', getUsers);
+router.get('/:id', getUserById);
+router.put('/:id', validate(userUpdatePayloadSchema), editUser);
+router.delete('/:id', deleteUser);
 
 export default router;
