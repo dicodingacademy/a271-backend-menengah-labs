459b69f0a42048dbe1721c136124bd7938f9480a
arasopraza <aras.opraza@gmail.com>
2025-09-18 21:08:28 +0700

    fix: server config

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import server from './server/index.js';
 
-const port = process.env.PORT || 3000;
+const port = 3000;
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running at http://${host}:${port}`);
 });
