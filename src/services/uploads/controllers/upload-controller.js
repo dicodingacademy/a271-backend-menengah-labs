de96c2125a2e0bca019c425133984e5ea675bc97
arasopraza <aras.opraza@gmail.com>
2025-10-20 13:40:45 +0700

    refactor: correct import path for authentication repositories and clean up upload controller

--- src/services/uploads/controllers/upload-controller.js
+++ src/services/uploads/controllers/upload-controller.js
@@ -2,8 +2,6 @@ import ClientError from '../../../exceptions/client-error.js';
 import response from '../../../utils/response.js';
 import StorageService from '../storage/storage-service.js';
 
-const storageService = new StorageService();
-
 export const uploadImages = async (req, res, next) => {
   if (!req.file) {
     return next(new ClientError('No file uploaded'));
