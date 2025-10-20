456803a1f418cd3dac15b3b7ed5ab997a5db335d
arasopraza <aras.opraza@gmail.com>
2025-10-20 13:10:34 +0700

    fix: load environment variables in server configuration

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import server from './server/index.js';
 
 const port = process.env.PORT;
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import routes from '../routes/index.js';
 import ErrorHandler from '../middlewares/error.js';
-import 'dotenv/config';
 
 const app = express();
 
