fd7d138099a82fd1f09bccedf0ccc897e20ee239
arasopraza <aras.opraza@gmail.com>
2025-10-20 13:41:58 +0700

    refactor: correct import statement for authentication repositories and simplify storage service import

--- src/services/uploads/controllers/upload-controller.js
+++ src/services/uploads/controllers/upload-controller.js
@@ -1,8 +1,6 @@
 import ClientError from '../../../exceptions/client-error.js';
 import response from '../../../utils/response.js';
-import StorageService from '../storage/storage-service.js';
-
-const storageService = new StorageService();
+import storageService from '../storage/storage-service.js';
 
 export const uploadImages = async (req, res, next) => {
   if (!req.file) {
