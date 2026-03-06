bbdf25d6accadb895aba6ba57f3eed2b5ed959a5
Arsy Opraza Akma <34035232+arasopraza@users.noreply.github.com>
2026-03-06 10:17:49 +0700

    chore: move folder to notes-app-back-end

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import server from './server/index.js';
-
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
